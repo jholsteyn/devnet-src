a232b575b201baf393387d6f6d6b220dfd3388fb
jholsteyn <holsteynjohan@hotmail.com>
2021-04-05 19:46:25 +0200

    changes in myLocation

--- python/myLocation.py
+++ python/myLocation.py
@@ -2,8 +2,15 @@ class Location:
     def __init__(self, name, country):
         self.name = name
         self.country = country
+        
+    def myLocation(self):
+        print("Hi, my name is " + self.name + " and I live in " + self.country + ".")
 
-loc = Location("Johan", "Belgium")
-print(loc.name)
-print(loc.country)
-print(type(loc))
+#First instantiation of the class Location
+loc1 = Location("Tomas", "Portugal")
+loc2 = Location("Ying", "China")
+loc3 = Location("Amare", "Kenya")
+#Call a method from the intstantiated class
+loc1.myLocation()
+loc2.myLocation()
+loc3.myLocation()
