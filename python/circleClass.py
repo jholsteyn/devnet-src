fdc8c8c3ab225a951173a8e8534dba5bb5bf1bb0
jholsteyn <holsteynjohan@hotmail.com>
2021-04-05 20:39:03 +0200

    corrections to Circle class

--- python/circleClass.py
+++ python/circleClass.py
@@ -1,10 +1,12 @@
 # Given a radius value, print the circumference of a circle.
 # Formula for a circumference is c = pi * 2 * radius.
+
 class Circle:
+
     def __init__(self, radius):
         self.radius = radius
 
-    def cicumference(self):
+    def circumference(self):
         pi = 3.14
         circumferenceValue = pi * self.radius * 2
         return circumferenceValue
